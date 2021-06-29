c761609c557c12d04eacd3ffe7da1bb7c197c591
hoangnguyen <nguyenhd01012000@gmail.com>
2021-06-29 17:23:16 +0700

    add

--- apps/email/models.py
+++ apps/email/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Email(models.Model):
+    name = models.TextField(blank=True, null=True)
+    email = models.EmailField(max_length=254)
+    review = models.TextField(blank=True, null=True)
--- apps/email/views.py
+++ apps/email/views.py
@@ -1,3 +1,4 @@
+from apps.email.models import Email
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
@@ -6,6 +7,8 @@ from .tasks import send_review_email_task
 @api_view(['GET', 'POST'])
 def send_subscription_email(request):
     if request.method == 'POST':
+        email = Email(name=request.data['name'],email=request.data['email'],review=request.data['review'])
+        email.save()
         msg = "Thank for your review !!!"
         send_review_email_task.delay(request.data['name'],request.data['email'],request.data['review'])
         return HttpResponse(msg)
